d16a759f2bef516d6ee3d8186284b246dcf026ee
João Pedro Alves <joaopealves.dev@gmail.com>
2025-09-22 15:45:57 -0300

    chore(dependencies): update package versions and improve schema validation

--- src/infra/http/schemas/bookmarks-schema.ts
+++ src/infra/http/schemas/bookmarks-schema.ts
@@ -1,15 +1,9 @@
 import { z } from 'zod'
 
 export const createBookmarkBodySchemaRequest = z.object({
-  workspaceId: z
-    .string()
-    .uuid('Workspace ID must be a valid UUID')
-    .min(1, 'Workspace ID is required.'),
-  memberId: z
-    .string()
-    .uuid('Member ID must be a valid UUID')
-    .min(1, 'Member ID is required.'),
-  url: z.string().url('URL must be a valid URL').min(1, 'URL is required.'),
+  workspaceId: z.uuid('Workspace ID must be a valid UUID'),
+  memberId: z.uuid('Member ID must be a valid UUID'),
+  url: z.string().url('URL must be a valid URL'),
 })
 
 export const createBookmarkBodySchemaResponse = {
