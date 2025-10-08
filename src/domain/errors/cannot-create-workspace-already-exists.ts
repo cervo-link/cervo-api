40c174f0077c0c369f057ba27e63415c5cc94ca7
João Pedro Alves <joaopealves.dev@gmail.com>
2025-10-07 23:34:50 -0300

    feat(errors): add CannotCreateWorkspaceAlreadyExists error class and update workspace creation error handling

--- src/domain/errors/cannot-create-workspace-already-exists.ts
+++ src/domain/errors/cannot-create-workspace-already-exists.ts
@@ -1,6 +1,6 @@
 import { DomainError } from './domain-error'
 
-export class CannotCreateWorkspaceDueConstraintError extends DomainError {
+export class CannotCreateWorkspaceAlreadyExists extends DomainError {
   constructor(message?: string) {
     super(message ?? 'Cannot create workspace due constraint', 422) //  Unprocessable Entity
   }
